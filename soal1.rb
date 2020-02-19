25e549a53ee95aef5f74e5c0d5e67d2949b80695
Alwiaulian <alwi.nuryadin@gmail.com>
2020-02-19 16:09:18 -0500

    membuat soal ke 1

---
+++ soal1.rb
@@ -0,0 +1,13 @@
+#Konversi menit ke jam
+#contoh 
+# 63 = 1:03 (satu menit tiga detik)
+
+def konversiMenit(menit)
+# Area kode nya 
+
+end
+#hasilnya harus seperti dibawah ini
+konversiMenit(32) # 0:32
+konversiMenit(63) # 1:03
+konversiMenit(133) # 2:13
+konversiMenit(200) # 3.20
