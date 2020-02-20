3e1d7661f54fc93a1f18ebf86fca0f0f75397056
akbaryusufsaputra <akbar.yusuf1@gmail.com>
2020-02-20 14:02:17 +0700

    akbaryusuf

--- jawabansoal1.rb
+++ jawabansoal1.rb
@@ -1,7 +1,12 @@
 def konversiMenit(menit) 
   jam = menit / 60
   tmp = menit % 60
-  puts "#{jam}:#{tmp}"
+
+  if  tmp < 10
+  puts "#{jam}:0#{tmp}"
+  else
+    puts "#{jam}:#{tmp}"
+  end
 end
   print "input menit: "
   menit = gets.chomp.to_i
