40fee97469c7597647e167bdff48131e6d066a51
faridmahindra <faridmahindra@gmail.com>
2020-02-25 13:40:35 +0700

    exam1

---
+++ ulangan2.rb
@@ -0,0 +1,13 @@
+array1 = [1,2,3,4,5,6,7,8,9]
+array2 = [1,7,6,0]
+jmlrotasi = 3
+jmlrotasi.times do
+    array1.unshift(array1.last)
+    array1.pop(1)
+    array2.unshift(array2.last)
+    array2.pop(1)
+end
+puts "======= Array 1 ========"
+print array1
+puts "======= Array 2 ========"
+puts array2
