ccfa16d88dd9031ee73b788d7128a5afb994a67c
akbaryusufsaputra <akbar.yusuf1@gmail.com>
2020-02-25 13:27:43 +0700

    punyaakbar

---
+++ jawaban3.rb
@@ -0,0 +1,34 @@
+buah = {apel:5000,nanas:10000,semangka:20000,jeruk:4000,anggur:6000}
+
+puts "Masukkan jumlah apel"
+apel = gets.chomp.to_i
+puts "Masukkan jumlah semangka"
+semangka = gets.chomp.to_i
+
+jumlahA = apel * buah[:apel]
+jumlahS = semangka*buah[:semangka]
+
+total = jumlahA + jumlahS
+
+if total >= 50000
+    hasil_akhir = total-(total*10/100)
+else
+    hasil_akhir = total
+end
+
+
+if jumlahS = 0
+    puts "-------buah yang dibeli-----"
+    print apel ," apel = " , jumlahA
+    puts " "
+    puts "-----------total harga------"
+    puts hasil_akhir
+else
+puts "-------buah yang dibeli-----"
+print apel ," apel = " , jumlahA
+puts " "
+print semangka , " semangka = " , jumlahS
+puts " "
+puts "-----------total harga------"
+puts hasil_akhir
+end
